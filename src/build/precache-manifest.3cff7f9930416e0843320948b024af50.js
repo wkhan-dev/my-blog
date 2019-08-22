36c4fb9ad836b3c72675dd5d776a1f025bec526c
wkhan-dev <wahaj.ah.khan@gmail.com>
2019-08-21 22:47:00 -0400

    First commit

---
+++ src/build/precache-manifest.3cff7f9930416e0843320948b024af50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eacfdd9577b2a9652f7818d6c9495cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d894616d2ad0d2a61a77",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "d212323b7b89d0526861",
+    "url": "/static/js/2.8a17f4b0.chunk.js"
+  },
+  {
+    "revision": "d894616d2ad0d2a61a77",
+    "url": "/static/js/main.d7c84389.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
